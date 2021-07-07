358f01a260928d95d2d5d36f7d8ccd6fd5cc8151
zhaoyunxing <2385585770@qq.com>
2021-07-07 09:55:38 +0800

    up:node name

--- k8s/docker_install.sh
+++ k8s/docker_install.sh
@@ -30,8 +30,8 @@ sudo systemctl enable chronyd
 # hosts设置
 sudo sh -c "echo -e '
 192.168.56.100 k8s-master
-192.168.56.101 k8s-node-1
-192.168.56.102 k8s-node-2' > /etc/hosts"
+192.168.56.101 k8s-node1
+192.168.56.102 k8s-node2' > /etc/hosts"
 
 # 关闭iptables、firewalld
 sudo systemctl stop firewalld
@@ -46,3 +46,5 @@ SELINUX=disabled
 SELINUXTYPE=targeted' > /etc/selinux/config"
 
 # 禁止swap分区
+sudo swapoff -a
+sudo sed -i '/swap/s/^/#/g' /etc/fstab
