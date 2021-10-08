e7ebf7b78779c13855c82f210845440f04aae8cf
zhaoyunxing <zhaoyunxing@apache.org>
2021-10-08 15:09:48 +0800

    修改master初始化脚本

--- k8s/script/master_init.sh
+++ k8s/script/master_init.sh
@@ -2,9 +2,8 @@
 # author:zhaoyunxing
 
 # init master
-if [ !  -d ".kube" ]; then
+if [ ! -d ".kube" ]; then
 echo "======== start kubeadm init ==========="
-
 sudo kubeadm init \
     --apiserver-advertise-address=192.168.56.200 \
     --image-repository registry.aliyuncs.com/google_containers \
@@ -14,12 +13,13 @@ mkdir -p $HOME/.kube
 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
 
-sudo sh -c "echo 'export KUBECONFIG=/etc/kubernetes/admin.conf' >> /etc/profile"
 source /etc/profile
 # 部署flannel网络
 kubectl apply -f /vagrant/config/kube-flannel.yml
 # # 文件授权
+# sudo sh -c "echo 'export KUBECONFIG=/etc/kubernetes/admin.conf' >> /etc/profile"
 # sudo chmod 666 /etc/kubernetes/admin.conf
 # 设置自动补全
-sudo sh -c "echo 'source <(kubectl completion bash)' >> ~/.bashrc" # 在您的 bash shell 中永久的添加自动补全
+# echo "source <(kubectl completion bash)" >> ~/.bashrc
+sudo sh -c "echo -e 'source <(kubectl completion bash)' >> .bashrc" # 在您的 bash shell 中永久的添加自动补全
 fi
