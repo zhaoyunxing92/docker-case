9b225b662e7dac6b8b33b2df3d1f82cb65527bb1
zhaoyunxing <zhaoyunxing@apache.org>
2021-10-08 10:10:18 +0800

    修改初始化脚本

--- k8s/script/master_init.sh
+++ k8s/script/master_init.sh
@@ -2,18 +2,22 @@
 # author:zhaoyunxing
 
 # init master
-# sudo kubeadm init \
-#     --apiserver-advertise-address=192.168.56.100 \
-#     --image-repository registry.aliyuncs.com/google_containers \
-#     --pod-network-cidr=10.244.0.0/16
+if ! type kubeadm >/dev/null 2>&1; then
+echo "======== start init master ==========="
 
-# hosts设置
-# sudo sh -c "echo -e '
-# 192.168.56.200 k8s-master
-# 192.168.56.201 k8s-node1
-# 192.168.56.202 k8s-node2' >> /etc/hosts"
+sudo kubeadm init \
+    --apiserver-advertise-address=192.168.56.200 \
+    --image-repository registry.aliyuncs.com/google_containers \
+    --pod-network-cidr=10.244.0.0/16
 
-# sudo sh -c "echo 'export KUBECONFIG=/etc/kubernetes/admin.conf' >> /etc/profile"
-# source /etc/profile
+mkdir -p $HOME/.kube
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
+
+sudo sh -c "echo 'export KUBECONFIG=/etc/kubernetes/admin.conf' >> /etc/profile"
+source /etc/profile
 # # 文件授权
 # sudo chmod 666 /etc/kubernetes/admin.conf
+# 设置自动补全
+sudo sh -c "echo 'source <(kubectl completion bash)' >> ~/.bashrc" # 在您的 bash shell 中永久的添加自动补全
+fi
