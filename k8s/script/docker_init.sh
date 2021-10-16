634cf47b7cc2645273d2a49ab2d7cb30bf896d5a
zhaoyunxing <zhaoyunxing@apache.org>
2021-10-16 14:08:55 +0800

    up:更新docer镜像源

--- k8s/script/docker_init.sh
+++ k8s/script/docker_init.sh
@@ -6,7 +6,7 @@ if ! type docker >/dev/null 2>&1; then
     # 安装更新驱动包
     sudo yum install -y yum-utils device-mapper-persistent-data lvm2
     # 设置repo
-    sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+    sudo yum-config-manager --add-repo http://mirrors.aliyun.com/docker-ce/linux/centos/docker-ce.repo
     # 安装docker
     sudo yum install -y docker-ce docker-ce-cli containerd.io
     # 启动docker
