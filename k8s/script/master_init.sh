33544910ef4d4efaae3a20da0335aee01db3344f
zhaoyunxing <zhaoyunxing@apache.org>
2022-06-04 16:14:32 +0800

    指定k8s版本

--- k8s/script/master_init.sh
+++ k8s/script/master_init.sh
@@ -3,19 +3,15 @@
 
 # init master
 if [ ! -d ".kube" ]; then
-echo "======== master start kubeadm init ==========="
-sudo yum install -y kubelet kubeadm kubectl ipvsadm
+echo "======== master start init v1.23.6 kubeadm ==========="
+sudo yum install -y kubelet-1.23.6-0 kubectl-1.23.6-0 kubeadm-1.23.6-0 ipvsadm
 sudo systemctl enable kubelet && sudo systemctl start kubelet
-
-# fix unknown service runtime.v1alpha2.RuntimeService
-# https://github.com/containerd/containerd/issues/4581
-sudo rm /etc/containerd/config.toml
-sudo systemctl restart containerd
 
 sudo kubeadm init \
     --apiserver-advertise-address=192.168.56.200 \
     --image-repository registry.aliyuncs.com/google_containers \
-    --pod-network-cidr=10.244.0.0/16
+    --pod-network-cidr=10.244.0.0/16 \
+    --kubernetes-version=v1.23.6
 
 sudo mkdir -p $HOME/.kube
 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
--- k8s/script/node_init.sh
+++ k8s/script/node_init.sh
@@ -5,7 +5,7 @@
 # # hosts设置
 if ! type kubelet >/dev/null 2>&1; then
 
-sudo yum install -y kubelet kubeadm
+sudo yum install -y kubelet-1.23.6-0
 sudo systemctl enable kubelet && sudo systemctl start kubelet
 
 fi
