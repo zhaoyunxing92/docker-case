05a3d8af377596a677cd97116ea43e98db6c23d7
zhaoyunxing <zhaoyunxing@apache.org>
2022-06-19 20:27:07 +0800

    up docker compose version

--- docker/script/docker_init.sh
+++ docker/script/docker_init.sh
@@ -25,6 +25,6 @@ fi
 
 if ! type docker-compose >/dev/null 2>&1; then
   echo "======== start install docker-compose ==========="
-  sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+  sudo curl -L "https://github.com/docker/compose/releases/download/v2.6.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
   sudo chmod +x /usr/local/bin/docker-compose
 fi
